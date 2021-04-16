801df578dbf1e527dd37a2b7554913c094e51f00
colorfulberry <senluowanxiangt@gmail.com>
2021-04-16 15:20:11 +0800

    Update paginator.rb

--- kaminari-core/lib/kaminari/helpers/paginator.rb
+++ kaminari-core/lib/kaminari/helpers/paginator.rb
@@ -26,7 +26,7 @@ module Kaminari
 
       # render given block as a view template
       def render(&block)
-        instance_eval(&block) if @options[:total_pages] > 1
+        instance_eval(&block) if @options[:total_pages] > 1 || @options[:always_show]
 
         # This allows for showing fall-back HTML when there's only one page:
         #
